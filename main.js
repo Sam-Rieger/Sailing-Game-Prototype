06e88b677d976e6c26c7d8c2e03125af3b94d01e
Samuel Rieger <samueljrieger@gmail.com>
2023-10-31 12:08:23 -0600

    began rebuilding asynchronous infrastructure, added debugging

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const game = require('./modules/game_utilities.js');
 const UI = require('./modules/UI_utilities.js');
 
-//UI.clear_terminal();
-UI.display_UI_options_tabs();
+UI.clear_terminal();
+console.log("hi1");
 
-if(UI.yes_or_no_question('Do you like javascript? [y|n]\n') == 1) {
+let user_response;
+UI.display_UI_options_tabs(function selection(response) {user_response = response});
+
+
+//if(UI.yes_or_no_question('Do you like javascript? [y|n]\n') == 1) {
     
-}
+//}
--- modules/UI_utilities.js
+++ modules/UI_utilities.js
@@ -3,43 +3,44 @@ const termkit = require( 'terminal-kit' );
 
 // example function that displays a question, and returns the user's
 // response as 1 or 0.  Takes question text as an argument.
-exports.yes_or_no_question = function (question_text) {
+exports.yes_or_no_question = function (question_text, returnFunction) {
 	terminal.blue( question_text ) ;
 
 	terminal.yesOrNo( { yes: [ 'y' , 'ENTER' ], no: [ 'n' ] } , function( error , result ) {
 	
 		if ( result ) {
 			terminal.green( "'Yes'\n" );
-            process.exit();
-            return 1;
+			terminal.processExit();
 		}
 		else {
 			terminal.red( "'No'\n" );
-            process.exit();
-            return 0;
+			terminal.processExit();
 		}
 	});
 }
 
 //clear terminal
-exports.clear_terminal() = function() {
+exports.clear_terminal = function () {
 	terminal.clear();
 }
 
 //display the options a player has
-exports.display_UI_options_tabs = function () {
+exports.display_UI_options_tabs = function (returnFunction) {
 
 	var menu_options = ["Option1", "Option2", "Option3", "Option4"];
 
 	var display_format = {
 		y: 1,
 		style: terminal.inverse,
-		selectedStyle: terminal.dim.blue. bggreen
+		selectedStyle: terminal.dim.blue.bgGreen
 	};
 
 	terminal.singleLineMenu(menu_options, display_format, function( error , response ) {
-			terminal.red(response.selectedText);
-			process.exit();
+			//terminal.red(response.selectedText);
+			//console.log(response.selectedIndex);
+			//process.exit();
+			returnFunction(response);
+			terminal.processExit();
 		}
 	);
 
